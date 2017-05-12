fcfb85c28e126764c4776d205f689de25a2a1215
Marjorie Hahn <mah479@students.froot.nau.edu>
2017-05-11 19:10:24 -0700

    Shooting method.

--- shooting_method_driver.m
+++ shooting_method_driver.m
@@ -1,20 +1,44 @@
-a = 0;
-b = 1;
+A = 0;
+B = 50;
 N = 100;
 
-c = 0;
-d = 1;
-eqn1 = @(y, yp) yp;
-eqn2 = @(y, yp) -y.^3;
-t = linspace(a, b, N+1)';
+s = linspace(A, B, N+1);
 
-diff = 1;
-while diff > 0
-    [y1, y2] = rk_mod(eqn1, eqn2, t, c, d);
-    plot(t, y1);
-    hold on;
-    d = d + 0.1; % increase d slightly
-    diff = y1(N+1);
-    % print in table format for LaTex
-    fprintf('%g & %f \\\\ \\hline\n', d, y1(N+1));
+D = 1:N+1;
+
+last = 0; % saves the index of the last eigenvalue
+figure();
+
+for i=1:N+1
+
+    a = 0;
+    b = 1;
+    n = 60;
+
+    eqn1 = @(y, yp) yp;
+    eqn2 = @(y, yp) -y.^3 - s(i) .* y;
+    t = linspace(a, b, n+1)';
+    
+    for d=0:0.1:200
+        
+        % solve the IVP
+        [y1, y2] = rk_mod(eqn1, eqn2, t, 0, d);
+        
+        % if the result solves the BVP, save the solution
+        if y1(n) * y1(n+1) < 0
+            D(i) = d;
+            break;
+        end
+    end
+    
+    % if we hit an eigenvalue, plot the branch for the eigenfunction
+    if abs(D(i)) <= 0.1
+        plot(s(last+1:i), D(last+1:i));
+        hold on;
+        last = i;
+    end
 end
+% plot the remaining branch as well, if needed
+plot(s(last+1:end),D(last+1:end));
+
+
